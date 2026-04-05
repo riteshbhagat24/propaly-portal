9d47e7cfc7e13cf9a4cabe949ef4e113c691204d
riteshbhagat24 <ritesh.bbhagat@gmail.com>
2026-04-05 20:28:26 +0530

    Fix: logo served from public folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="flex items-center">
-            <img src="https://drive.google.com/uc?export=view&id=1gzFsweqCOwmY4N403tvu14j9_GmSox98" alt="Propaly Realtors" className="h-10 w-auto object-contain" />
+            <img src="/logo.png" alt="Propaly Realtors" className="h-10 w-auto object-contain" />
           </Link>
           <div className="hidden md:flex items-center gap-1">
             {Object.keys(menuItems).map(item => (
